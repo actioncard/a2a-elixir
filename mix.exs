6b9d3eea19adb0fb61dd3f3c7a54d21601057728
Max Ekman <max@looplab.se>
2026-03-03 11:32:55 +0100

    build: update package version to v0.1.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule A2A.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @source_url "https://github.com/actioncard/a2a-elixir"
   @a2a_spec_url "https://google.github.io/A2A/"
 
